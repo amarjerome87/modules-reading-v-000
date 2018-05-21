eb56870df4879f1b4c568e15b91aef4ce163059a
Amar Chatman <amarjerome@gmail.com>
2018-05-21 20:14:21 +0000

    automatically backed up by learn

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -1,8 +1,10 @@
 require_relative './class_methods_module.rb'
 require_relative './dance_module.rb'
-class Dancer
+  class Dancer
 
-  include Dance 
+  extend MetaDancing
+
+  include Dance
 
   attr_accessor :name
 
